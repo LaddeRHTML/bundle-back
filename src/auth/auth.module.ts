c8d5f2261fd984fc9a7fdd0be1d7e3613933d70c
Roman Pshenichnyy <43478763+LaddeRHTML@users.noreply.github.com>
2022-10-01 18:22:18 +0600

    bundle-system-11: fixed auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'api/users/users.module';
+import { jwtConstants } from 'src/constants/jwt-const';
 
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt-auth.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-    imports: [
-        UsersModule,
-        PassportModule,
-        JwtModule.register({
-            secret: `${process.env.SECRET_KEY}`,
-            signOptions: { expiresIn: `${process.env.TOKEN_EXPIRATION_TIME}` }
-        })
-    ],
+    imports: [UsersModule, PassportModule, JwtModule.register(jwtConstants)],
     providers: [AuthService, LocalStrategy, JwtStrategy],
     exports: [AuthService]
 })
